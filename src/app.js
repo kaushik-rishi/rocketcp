2dfa65978fb4dab7845d15075c3a599ecf8c4931
Shubham-Kumar-2000 <shukhu10@gmail.com>
2021-03-13 20:28:47 +0530

    cli argument rcp added

---
+++ src/app.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict'
+
+const { ArgumentParser } = require('argparse')
+const { version } = require('../package.json')
+
+const parser = new ArgumentParser({
+  description:
+    'A command line client for parsing and making folders and files for test cases using the competitive companion extension and testing the solution. Building this with an aim of making Competitive Programming More Productive'
+})
+
+parser.add_argument('-v', '--version', { action: 'version', version })
+parser.add_argument('mode', {
+  help: 'Allows the user to setup RCP properly',
+  type: String,
+  choices: ['init', 'server'],
+  nargs: '?',
+  default: 'server'
+})
+
+let args = parser.parse_args()
+if (args.mode === 'server') {
+  require('./server')
+}
+if (args.mode === 'init') {
+  console.log('We are working on it')
+}

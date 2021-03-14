1b02565e780451440ae2866c8e28070b105f8acb
kaushik-rishi <rishi.cp01@gmail.com>
2021-03-14 06:38:56 +0530

    basic refactoring

--- src/executor.js
+++ src/executor.js
@@ -1,20 +1,19 @@
-const shell = require('shelljs')
-const Joi = require('joi')
+const chalk = require('chalk')
 const fs = require('fs')
+const Joi = require('joi')
+const shell = require('shelljs')
+
 const root = process.cwd()
-const Red = '\x1b[31m'
-const NC = '\x1b[0m'
-const Blue = '\x1b[34m'
-const Yellow = '\x1b[33m'
-const Green = '\x1b[32m'
+
 let config = null
+
 try {
   config = require(`${root}/.config.json`)
-} catch (e) {
+} catch (err) {
   if (fs.existsSync(`${root}/.config.json`)) {
-    console.log(`${Red}RCP reserved file .config.json is corupted.${NC}`)
+    console.log(chalk.red('RCP reserved file .config.json is corupted.'))
   } else {
-    console.log(`${Red}This is not a valid RCP problem directory.${NC}`)
+    console.log(chalk.red('This is not a valid RCP problem directory.'))
   }
   process.exit(1)
 }
@@ -23,49 +22,67 @@ function rtrim(str) {
   if (str == null) return str
   return str.replace(/\s+$/g, '')
 }
+
 function match(sol, test) {
   if (!sol) return false
   sol = sol.split('\n')
   test = test.split('\n')
+
   if (sol.length < test.length) return false
+
   for (let i = 0; i < test.length; i += 1) {
     if (rtrim(sol[i]) != rtrim(test[i])) return false
   }
+
   return true
 }
+
 const execute = (lang) => {
   if (!config.langs[lang]) {
     console.log(
-      `${Red}This language is not added to this problem\n\n${Blue}Run : rcp --add=${Yellow}<Your Lang>\n${NC}To add a specific language to this problem.`
+      `${chalk.red('This language is not added to this problem')}
+ ${chalk.blue('Run : rcp --add=')}${chalk.yellow('<Your Lang>')}
+ To add a specific language to this problem.`
     )
   }
+
   const exeComands = config.langs[lang]
   let res = {}
+
   if (exeComands.requireCompilation) {
-    console.log(`${Blue}Compiling...${NC}`)
+    console.log(chalk.blue('Compiling...'))
     res = shell.exec(exeComands.compile)
+
     if (res.code !== 0) {
-      console.log(`${Red}Compilation Failed.${NC}`)
+      console.log(chalk.red('Compilation Failed.'))
       return
     }
   }
-  console.log(`${Green}Compiled Successfully.\n\n${Blue}Executing...`)
+
+  console.log(
+    `${chalk.green('Compiled Successfully.')} \n\n${chalk.blue('Executing...')}`
+  )
+
   config.testCases.forEach((t, i) => {
     i += 1
-    console.log(`${NC}Test Case ${i}: \n${NC}`)
+    console.log(`Test Case ${i}: \n`)
+
     res = shell.exec(`${exeComands.run} < ${t.input}`, { silent: true })
+
     if (res.code !== 0) {
-      console.log(`${Red}Run time error !!!\n\n${NC}${res.stderr}`)
-      return
+      return console.log(chalk.red('Run time error !!!\n\n') + res.stderr)
     }
+
     const out = fs.readFileSync(`${root}/${t.output}`).toString()
-    if (match(out, res.stdout)) {
-      console.log(`${Green}Passed Succesfully\n${NC}`)
-    } else {
+
+    if (match(out, res.stdout)) console.log(chalk.green('Passed Succesfully\n'))
+    else
       console.log(
-        `${Red}Failed\n${NC}\nExpected Output :\n${out}\nRecieved Output :\n${res.stdout}`
+        `${chalk.red(
+          'Failed'
+        )}\n\nExpected Output :\n${out}\nRecieved Output :\n${res.stdout}`
       )
-    }
+
     delete res
   })
 }

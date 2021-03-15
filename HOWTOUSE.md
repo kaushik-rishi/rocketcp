48338fa0321daea7cb37d8c11b420a3cc337438f
kaushik-rishi <rishi.cp01@gmail.com>
2021-03-15 11:21:34 +0530

    BUGFIX:add shelljs to package.json

--- HOWTOUSE.md
+++ HOWTOUSE.md
@@ -1,45 +1,45 @@
 ### How this tool can help the user?
 
-- Rocket Competitive Programming is a command line client for parsing and making folders and files for test cases using the `Competitive companion extension` and testing the solution. It is built with an 🎯 aim of making Competitive Programming more productive.
+-   Rocket Competitive Programming is a command line client for parsing and making folders and files for test cases using the `Competitive companion extension` and testing the solution. It is built with an 🎯 aim of making Competitive Programming more productive.
 
-- The tool creates a folder having all the problems of a particular codeforces round on whose page the user clicked the `Competitive Companion Extension`.
+-   The tool creates a folder having all the problems of a particular codeforces round on whose page the user clicked the `Competitive Companion Extension`.
 
 ### Who can use this tool?
 
-- The tool requires the extension to be installed on chrome first.
+-   The tool requires the extension to be installed on chrome first.
 
 ### How to setup this tool ?
 
-- Prerequisites to setup this tool
+-   Prerequisites to setup this tool
 
-  - `Node JS`
-    - [Install it here](https://nodejs.org/en/)
-  - `npm` (comes pre installed when you install node js)
+    -   `Node JS`
+        -   [Install it here](https://nodejs.org/en/)
+    -   `npm` (comes pre installed when you install node js)
 
-- Install the browser addon `Competitive Companion` (Available for both chrome and firefox)
+-   Install the browser addon `Competitive Companion` (Available for both chrome and firefox)
 
-  [Google Chrome Addon](https://chrome.google.com/webstore/detail/competitive-companion/cjnmckjndlpiamhfimnnjmnckgghkjbl)
-  [Firefox Addon](https://addons.mozilla.org/en-US/firefox/addon/competitive-companion/)
+    [Google Chrome Addon](https://chrome.google.com/webstore/detail/competitive-companion/cjnmckjndlpiamhfimnnjmnckgghkjbl)
+    [Firefox Addon](https://addons.mozilla.org/en-US/firefox/addon/competitive-companion/)
 
-- Clone the repository into any directory of you interest
+-   Clone the repository into any directory of you interest
 
 ```sh
 git clone https://github.com/kaushik-rishi/rocketcp.git
 ```
 
-- Navigate into the directory `rocketcp`
+-   Navigate into the directory `rocketcp`
 
 ```sh
 cd rocketcp
 ```
 
-- Run the command to install package
+-   Run the command to install package
 
 ```sh
 npm install
 ```
 
-- Run the command to start the server
+-   Run the command to start the server
 
 ```sh
 npm start
@@ -51,18 +51,18 @@ Take a look at existing issues at [our issue page](https://github.com/kaushik-ri
 
 <<<<<<< HEAD
 
-- To create a new issue, visit [our issue page](https://github.com/kaushik-rishi/rocketcp/issues)
+-   To create a new issue, visit [our issue page](https://github.com/kaushik-rishi/rocketcp/issues)
 
-- Then click on new issue as shown in image.
+-   Then click on new issue as shown in image.
 
 ![NewIssue](https://github.com/GuptaTanisha/rocketcp/blob/newDocs/src/images/Issue_image.jpg)
 
-- # Add a suitable title and describe your issue. One can add screenshots or small gifs for demonstration. And then submit it.
-- To create a new issue, visit [our issue page](https://github.com/kaushik-rishi/rocketcp/issues)
+-   # Add a suitable title and describe your issue. One can add screenshots or small gifs for demonstration. And then submit it.
+-   To create a new issue, visit [our issue page](https://github.com/kaushik-rishi/rocketcp/issues)
 
-- Then click on new issue as shown in image.
+-   Then click on new issue as shown in image.
 
 ![NewIssue](./assets/images/Issue_image.jpg)
 
-- Add a suitable title and describe your issue. One can add screenshots or small gifs for demonstration. And then submit it.
-  > > > > > > > a372f9cf615e0a1b2a70542a368df094dbb7cdcd
+-   Add a suitable title and describe your issue. One can add screenshots or small gifs for demonstration. And then submit it.
+    > > > > > > > a372f9cf615e0a1b2a70542a368df094dbb7cdcd
--- src/executor.js
+++ src/executor.js
@@ -13,103 +13,109 @@ const problemDir = process.cwd()
 
 // trims the block of spaces at the end of a string
 function rtrim(str) {
-  if (str == null) return str
-  return str.replace(/\s+$/g, '')
+    if (str == null) return str
+    return str.replace(/\s+$/g, '')
 }
 
 // returns wether the solution matches the test or not
 function match(sol, test) {
-  // TODO : use better diff algorithm or diff package to show the difference
-  if (!sol) return false
+    // TODO : use better diff algorithm or diff package to show the difference
+    if (!sol) return false
 
-  sol = sol.split('\n')
-  test = test.split('\n')
+    sol = sol.split('\n')
+    test = test.split('\n')
 
-  if (sol.length < test.length) return false
+    if (sol.length < test.length) return false
 
-  for (let i = 0; i < test.length; i += 1)
-    if (rtrim(sol[i]) != rtrim(test[i])) return false
+    for (let i = 0; i < test.length; i += 1)
+        if (rtrim(sol[i]) != rtrim(test[i])) return false
 
-  return true
+    return true
 }
 
 // executes the program file feeding it in the test case files and matching the output against the output files
 function execute(lang) {
-  if (!commands[lang]) {
-    console.log(
-      `${chalk.red('This language is not added to this problem')}
+    if (!commands[lang]) {
+        console.log(
+            `${chalk.red('This language is not added to this problem')}
  ${chalk.blue('Run : rcp --add=')}${chalk.yellow('<Your Lang>')}
  To add a specific language to this problem.`
-    )
-  }
-
-  let commandsData = commands[lang]
-  let compileCommand = commandsData['compile']
-  let runCommand =
-    commandsData['run'][process.platform === 'win32' ? 'win32' : 'unix']
-
-  if (!runCommand.isinterpreted) {
-    console.log(chalk.blue('Compiling...'))
-    let compilationResult = sh.exec(compileCommand)
-
-    if (compilationResult.code !== 0)
-      return console.log(chalk.red('Compilation Failed.'))
-
-    console.log(chalk.green('Compiled Successfully.') + '\n')
-  }
-
-  let testCaseFiles = fs.readdirSync(path.join(problemDir, 'testcases'))
-  let inputFileRegex = new RegExp(/in([1-9])*\.txt/)
-  let inputFileIndices = []
-
-  testCaseFiles.forEach((fname) => {
-    let match = fname.match(inputFileRegex)
-    if (match) inputFileIndices.push(match[1])
-  })
-
-  console.log(chalk.cyan('Running the code against test cases ..\n'))
-
-  inputFileIndices.forEach((fileIndex) => {
-    let inpFileName = `in${fileIndex}.txt`
-    let opFileName = `out${fileIndex}.txt`
-    let result = sh.exec(
-      runCommand + '<' + path.join('testcases', inpFileName),
-      { silent: true }
-    )
-    let out = result.stdout
-
-    if (result.code !== 0) {
-      console.log(chalk.red.underline(`Runtime error on test #${fileIndex}`))
-      console.log(chalk.red('Error Message') + ' : ' + result.stderr + '\n')
-    } else {
-      if (fs.existsSync(path.join('testcases', opFileName))) {
-        let expectedOut = fs
-          .readFileSync(path.join('testcases', opFileName))
-          .toString()
-
-        if (match(expectedOut, out))
-          console.log(chalk.green(`Passed Test #${fileIndex}`))
-        else
-          console.log(
-            chalk.red(`Failed Test ${fileIndex}`) +
-              chalk.magentaBright('\nExpected Output :\n') +
-              expectedOut +
-              chalk.magentaBright('\nRecieved Output :\n') +
-              out
-          )
-      } else {
-        console.log(`\n` + chalk.underline(`Test ${fileIndex}`))
-        console.log(chalk.yellow('No output file found'))
-        console.log(chalk.blue('-------- INPUT --------'))
-        console.log(
-          fs.readFileSync(path.join('testcases', inpFileName)).toString()
         )
-
-        console.log(chalk.blue('-------- OUTPUT --------'))
-        console.log(out)
-      }
     }
-  })
+
+    let commandsData = commands[lang]
+    let compileCommand = commandsData['compile']
+    let runCommand =
+        commandsData['run'][process.platform === 'win32' ? 'win32' : 'unix']
+
+    if (!runCommand.isinterpreted) {
+        console.log(chalk.blue('Compiling...'))
+        let compilationResult = sh.exec(compileCommand)
+
+        if (compilationResult.code !== 0)
+            return console.log(chalk.red('Compilation Failed.'))
+
+        console.log(chalk.green('Compiled Successfully.') + '\n')
+    }
+
+    let testCaseFiles = fs.readdirSync(path.join(problemDir, 'testcases'))
+    let inputFileRegex = new RegExp(/in([1-9])*\.txt/)
+    let inputFileIndices = []
+
+    testCaseFiles.forEach((fname) => {
+        let match = fname.match(inputFileRegex)
+        if (match) inputFileIndices.push(match[1])
+    })
+
+    console.log(chalk.cyan('Running the code against test cases ..\n'))
+
+    inputFileIndices.forEach((fileIndex) => {
+        let inpFileName = `in${fileIndex}.txt`
+        let opFileName = `out${fileIndex}.txt`
+        let result = sh.exec(
+            runCommand + '<' + path.join('testcases', inpFileName),
+            { silent: true }
+        )
+        let out = result.stdout
+
+        if (result.code !== 0) {
+            console.log(
+                chalk.red.underline(`Runtime error on test #${fileIndex}`)
+            )
+            console.log(
+                chalk.red('Error Message') + ' : ' + result.stderr + '\n'
+            )
+        } else {
+            if (fs.existsSync(path.join('testcases', opFileName))) {
+                let expectedOut = fs
+                    .readFileSync(path.join('testcases', opFileName))
+                    .toString()
+
+                if (match(expectedOut, out))
+                    console.log(chalk.green(`Passed Test #${fileIndex}`))
+                else
+                    console.log(
+                        chalk.red(`Failed Test ${fileIndex}`) +
+                            chalk.magentaBright('\nExpected Output :\n') +
+                            expectedOut +
+                            chalk.magentaBright('\nRecieved Output :\n') +
+                            out
+                    )
+            } else {
+                console.log(`\n` + chalk.underline(`Test ${fileIndex}`))
+                console.log(chalk.yellow('No output file found'))
+                console.log(chalk.blue('-------- INPUT --------'))
+                console.log(
+                    fs
+                        .readFileSync(path.join('testcases', inpFileName))
+                        .toString()
+                )
+
+                console.log(chalk.blue('-------- OUTPUT --------'))
+                console.log(out)
+            }
+        }
+    })
 }
 
 // execute('cpp')

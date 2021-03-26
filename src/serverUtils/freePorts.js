a2c372b7e2eab4629bbd435b0ac8002ab3a5e453
kaushik-rishi <rishi.cp01@gmail.com>
2021-03-26 18:39:12 +0530

    BUGFIX: free ports

--- src/serverUtils/freePorts.js
+++ src/serverUtils/freePorts.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 
 const ports = [1327, 4244, 6174, 10042, 10043, 10045, 27121]
 
-const getPort = () =>
+module.exports.getPort = () =>
     Promise.all(
         ports.map((p) =>
             fp(p, p + 1)
@@ -18,13 +18,9 @@ const getPort = () =>
             return freePorts[0]
         })
         .catch((err) => {
-            console.log(chalk.red(e))
+            console.log(chalk.red(err))
             console.log(
                 '\n\nPlease Free any one of the bellow mentioned ports in your PC :\n'
             )
             ports.map((port) => console.log(`\n${chalk.blue('==>')} ${port}`))
         })
-
-getPort()
-
-module.exports = getPort

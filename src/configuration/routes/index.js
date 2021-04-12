dfc016ac013d950b44c62bbdb84d538a0645a86c
Shubham-Kumar-2000 <shukhu10@gmail.com>
2021-04-12 21:39:32 +0530

    Route to store Config

--- src/configuration/routes/index.js
+++ src/configuration/routes/index.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+
+const ROOT = path.join(os.userInfo().homedir, 'competitivecoding');
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', process.env.config);
+});
+
+router.post('/', (req, res) => {
+    try {
+        fs.writeFileSync(
+            path.join(ROOT, 'config.json'),
+            JSON.stringify(req.body, null, 4)
+        );
+        return res.status(200).json({
+            err: false
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(200).json({
+            err: true,
+            msg: String(e)
+        });
+    }
 });
 
 module.exports = (app) => {

56508f0c5f0a2fe190c36a528c5d6dee6c8d8065
Shubham-Kumar-2000 <shukhu10@gmail.com>
2021-05-30 15:43:59 +0530

    Added a intermediate template for java

---
+++ src/templates/codes/javaInter.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+public class Template
+{
+    static PrintWriter out=new PrintWriter((System.out));
+    public static void main(String args[])throws IOException
+    {
+        Reader sc=new Reader();
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            solve();
+        }
+	out.close();
+    }
+
+    public static void solve()
+    {
+    }
+    
+    static class Reader 
+    { 
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer("");
+        public String next()
+        {
+            while(!st.hasMoreTokens())
+            {
+                try
+                {
+                    st=new StringTokenizer(br.readLine());
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        public int nextInt()
+        {
+            return Integer.parseInt(next());
+        }
+        public long nextLong()
+        {
+            return Long.parseLong(next());
+        }
+        public double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+        public String nextLine()
+        {
+            try
+            {
+                return br.readLine();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        public boolean hasNext()
+        {
+            String next=null;
+            try
+            {
+                next=br.readLine();
+            }
+            catch(Exception e)
+            {
+            }
+            if(next==null)
+            {
+                return false;
+            }
+            st=new StringTokenizer(next);
+            return true;
+        }
+    } 
+}
